cda59c33d1bc7b9926144d2683e6555f2bac1260
Yirnick van Dijk <71263820+NitraatPlagiaat@users.noreply.github.com>
2025-07-21 12:55:50 +0200

    0.1-ALPHA-writing-rule-one-vowel-at-a-time

--- src/main/kotlin/controllers/InputController.kt
+++ src/main/kotlin/controllers/InputController.kt
@@ -1,16 +1,41 @@
 package controllers
 
 import models.getCorrespondingRune
+import models.checkExtraVowel
+import models.vowels
+
+var skipLetter = false
 
 fun input(text: String) {
     var futharkConversion = ""
-    for (letter in text) {
-        if (letter == ' ') {
+    for (letter in text.indices) {
+
+        if (skipLetter){
+            skipLetter = false
+            continue
+        }
+
+        if (text[letter] == ' ') {
             futharkConversion += " "
         }
         else {
-            if (letter in 'a'..'z') futharkConversion += getCorrespondingRune(letter)
+            if (text[letter] in 'a'..'z') {
+                futharkConversion += checkWritingRules(text, letter)
+            }
         }
     }
     println(futharkConversion)
+}
+
+fun checkWritingRules(text: String, index: Int): Char? {
+    val standardRune = getCorrespondingRune(text[index])
+    if (text[index] in vowels) {
+        val rune = checkExtraVowel(text, index)
+        if (rune != null) {
+            skipLetter = true
+            return rune
+        }
+    }
+
+    return standardRune
 }
